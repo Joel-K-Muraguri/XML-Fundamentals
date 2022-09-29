66cb16f9e6575e9ae1d93627e7bd362082c7c640
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2022-09-29 17:28:24 +0300

    navigation

--- navigation/src/main/java/com/joel/navigation/fragments/FavouritesFragment.kt
+++ navigation/src/main/java/com/joel/navigation/fragments/FavouritesFragment.kt
@@ -28,6 +28,8 @@ class FavouritesFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
--- navigation/src/main/java/com/joel/navigation/fragments/HomeFragment.kt
+++ navigation/src/main/java/com/joel/navigation/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.joel.navigation.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,6 +28,7 @@ class HomeFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -35,7 +37,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val homeButton : Button = root.findViewById<Button>(R.id.home_button)
+        root.setOnClickListener {
+
+        }
+        return root
     }
 
     companion object {
